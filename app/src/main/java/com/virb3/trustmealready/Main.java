ba0d55f08b906216e5a8696f23aa57f75711d4bb
Winhok <2063685743@qq.com>
2026-02-02 14:21:25 +0800

    add webview hook

--- app/src/main/java/com/virb3/trustmealready/Main.java
+++ app/src/main/java/com/virb3/trustmealready/Main.java
@@ -9,22 +9,36 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+import android.net.http.SslError;
+import android.net.http.X509TrustManagerExtensions;
+import android.util.Log;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebView;
+import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
+import static de.robv.android.xposed.XC_MethodReplacement.DO_NOTHING;
 import static de.robv.android.xposed.XposedHelpers.*;
 
-public class Main implements IXposedHookZygoteInit {
+public class Main implements IXposedHookZygoteInit, IXposedHookLoadPackage {
 
+    private static final String TAG = "TrustMeAlready";
     private static final String SSL_CLASS_NAME = "com.android.org.conscrypt.TrustManagerImpl";
     private static final String SSL_METHOD_NAME = "checkTrustedRecursive";
     private static final Class<?> SSL_RETURN_TYPE = List.class;
     private static final Class<?> SSL_RETURN_PARAM_TYPE = X509Certificate.class;
 
+    private static void log(String message) {
+        XposedBridge.log(TAG + ": " + message);
+        Log.d(TAG, message);
+    }
+
     @Override
     public void initZygote(StartupParam startupParam) throws Throwable {
-        XposedBridge.log("TrustMeAlready loading...");
+        log("TrustMeAlready loading...");
         int hookedMethods = 0;
 
         for (Method method : findClass(SSL_CLASS_NAME, null).getDeclaredMethods()) {
@@ -41,13 +55,52 @@ public class Main implements IXposedHookZygoteInit {
                 }
             });
 
-            XposedBridge.log("Hooking method:");
-            XposedBridge.log(method.toString());
+            log("Hooking method: " + method.toString());
             findAndHookMethod(SSL_CLASS_NAME, null, SSL_METHOD_NAME, params.toArray());
             hookedMethods++;
         }
 
-        XposedBridge.log(String.format(Locale.ENGLISH, "TrustMeAlready loaded! Hooked %d methods", hookedMethods));
+        log(String.format(Locale.ENGLISH, "TrustMeAlready loaded! Hooked %d methods", hookedMethods));
+    }
+
+    @Override
+    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
+        log("TrustMeAlready hooking WebView for: " + lpparam.packageName);
+
+        /* WebView Hooks */
+        /* frameworks/base/core/java/android/webkit/WebViewClient.java */
+        /* public void onReceivedSslError(Webview, SslErrorHandler, SslError) */
+        findAndHookMethod("android.webkit.WebViewClient", lpparam.classLoader, "onReceivedSslError",
+                WebView.class, SslErrorHandler.class, SslError.class, new XC_MethodReplacement() {
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        ((SslErrorHandler) param.args[1]).proceed();
+                        return null;
+                    }
+                });
+
+        /* frameworks/base/core/java/android/webkit/WebViewClient.java */
+        /* public void onReceivedError(WebView, int, String, String) */
+        findAndHookMethod("android.webkit.WebViewClient", lpparam.classLoader, "onReceivedError",
+                WebView.class, int.class, String.class, String.class, DO_NOTHING);
+
+        /* Additional hooks from JustTrustMe that affect WebView SSL validation */
+        /* X509TrustManagerExtensions */
+        try {
+            findAndHookMethod(X509TrustManagerExtensions.class, "checkServerTrusted", X509Certificate[].class, String.class, String.class, new XC_MethodReplacement() {
+                @Override
+                protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                    return param.args[0];
+                }
+            });
+        } catch (Throwable ignored) {
+        }
+
+        /* NetworkSecurityTrustManager */
+        try {
+            findAndHookMethod("android.security.net.config.NetworkSecurityTrustManager", lpparam.classLoader, "checkPins", List.class, DO_NOTHING);
+        } catch (Throwable ignored) {
+        }
     }
 
     private boolean checkSSLMethod(Method method) {
